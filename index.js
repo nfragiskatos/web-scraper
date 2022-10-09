e5949aa03b5c499ddd3cb42211a6fe3a3089090d
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-10-09 08:25:16 -0700

    added a couple restful endpoints to display results

--- index.js
+++ index.js
@@ -6,21 +6,27 @@ const express = require("express");
 const app = express();
 const url = `https://www.theguardian.com/uk`;
 
-axios(url)
-  .then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const articles = [];
-    $(".fc-item__title", html).each(function () {
-      const title = $(this).text();
-      const url = $(this).find("a").attr("href");
-      articles.push({
-        title,
-        url,
+app.get("/", (req, resp) => {
+  resp.json("This is my webscraper");
+});
+
+app.get("/results", (req, resp) => {
+  axios(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const articles = [];
+      $(".fc-item__title", html).each(function () {
+        const title = $(this).text();
+        const url = $(this).find("a").attr("href");
+        articles.push({
+          title,
+          url,
+        });
       });
-    });
-    console.log(articles.length);
-  })
-  .catch((e) => console.log(e));
+      resp.json(articles);
+    })
+    .catch((e) => console.log(e));
+});
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
