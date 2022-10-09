793b7ba68de71740e90a0664291330051ea603aa
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-10-09 08:17:41 -0700

    scraped basic article information for title and url

--- index.js
+++ index.js
@@ -4,5 +4,23 @@ const cheerio = require("cheerio");
 const express = require("express");
 
 const app = express();
+const url = `https://www.theguardian.com/uk`;
+
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+    $(".fc-item__title", html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find("a").attr("href");
+      articles.push({
+        title,
+        url,
+      });
+    });
+    console.log(articles.length);
+  })
+  .catch((e) => console.log(e));
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
